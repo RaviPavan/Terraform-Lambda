daa9aac2295e5ddd614016c2f177daeb86fa3990
ravipavan <ravipavan250@gmail.com>
2023-02-20 22:18:27 +0800

    Test

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ variable "aws_access_key" {}
 variable "aws_secret_key" {}
 
 provider "aws" {
-  region = "eu-west-1"
+  region = "eu-west-2"
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
 
